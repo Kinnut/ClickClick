09b5760f5e7834a3e81da4c64d0a9866e438a678
Kinnut <140511632+Kinnut@users.noreply.github.com>
2024-03-28 12:38:02 +0900

    노트 생성, 배치, 애니메이션

---
+++ Assets/1. Scripts/Note/NoteGroup.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteGroup : MonoBehaviour
+{
+    [SerializeField] private int noteMaxNum = 5;
+    [SerializeField] private GameObject notePref;
+    [SerializeField] private GameObject noteSpawn;
+    [SerializeField] private float noteGap = 6;
+
+
+    private List<Note> noteList = new List<Note>();
+
+    void Start()
+    {
+        for (int i = 0; i < noteMaxNum; i++)
+        {
+            GameObject noteGameObj = Instantiate(notePref);
+            noteGameObj.transform.SetParent(noteSpawn.transform);
+            noteGameObj.transform.localPosition = Vector3.up * noteList.Count * noteGap;
+            Note note = noteGameObj.GetComponent<Note>();
+
+            noteList.Add(note);
+        }
+    }
+}
